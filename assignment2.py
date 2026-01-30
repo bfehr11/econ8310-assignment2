e0998932f9e1fe5356225b864bbe735735ae6539
bfehr11 <89813070+bfehr11@users.noreply.github.com>
2026-01-30 16:09:29 -0600

    previously made the tjur scorer, never declared it as the scorer to use

--- assignment2.py
+++ assignment2.py
@@ -42,7 +42,7 @@ def tjurr(truth, pred):
 
 tjurr_scorer = make_scorer(tjurr, greater_is_better=True, response_method="predict_proba")
 
-model_tuned = RandomizedSearchCV(model, param_dist, n_iter=100, n_jobs=-1, cv=3)
+model_tuned = RandomizedSearchCV(model, param_dist, scoring=tjurr_scorer, n_iter=100, n_jobs=-1, cv=3)
 
 model_tuned.fit(x_train, y_train)
 
